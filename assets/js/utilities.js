229a0117df380fb0825a19c6be571a5460c8c806
EpicGazel <zaneshark@gmail.com>
2023-05-15 12:02:38 -0400

    Add "added_by_id" to parsed track
    Allows for distinction between who added what track.

--- assets/js/utilities.js
+++ assets/js/utilities.js
@@ -191,11 +191,11 @@ function rearrangeAdjacent(arr)
      
     for(var i = 0; i < arr.length; i++)
     {
-		console.log(arr[i].added_by.id)
-        if(mp.has(arr[i].added_by.id))
-          mp.set(arr[i].added_by.id, mp.get(arr[i].added_by.id)+1)
+		console.log(arr[i].added_by_id)
+        if(mp.has(arr[i].added_by_id))
+          mp.set(arr[i].added_by_id, mp.get(arr[i].added_by_id)+1)
         else
-          mp.set(arr[i].added_by.id, 1)
+          mp.set(arr[i].added_by_id, 1)
     }
      
     var pq = [];
@@ -204,7 +204,7 @@ function rearrangeAdjacent(arr)
     // in descending order
     for(var i = 0; i < arr.length; i++)
     {
-        var val = arr[i].added_by.id;
+        var val = arr[i].added_by_id;
          
         if (mp.get(val) > 0 && visited[val] != 1)
         {
