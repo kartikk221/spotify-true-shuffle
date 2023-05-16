7f4d372be0bd9941c4bf054aa4b526b3fc74a27e
EpicGazel <zaneshark@gmail.com>
2023-05-15 22:43:37 -0400

    Revert "Avoid adjacent songs added by same person"
    This reverts commit f9ab38a86806ef583367735fc219cdcc27413536.

--- assets/js/utilities.js
+++ assets/js/utilities.js
@@ -97,26 +97,16 @@ function clamp_string(string, length, trail = '...') {
  * @param {Array} array
  * @returns {Array}
  */
-function swap_shuffle(array, previous_adder) {
+function swap_shuffle(array) {
     // Handle scenario for less than 2 items
     if (array.length < 2) return array;
 
-	const maxRandAttempts = 5;
-
     // Perform a swap shuffle on the provided array
     for (let i = 0; i < array.length; i++) {
-		// Attempt 5 times to find unique adder for next track
-		for (let i = 0; i < maxRandAttempts; i++)
-		{
-			// Determine a random index with which we will swap current element
-			const rand = random_number(0, array.length - 1);
-			const temp = array[rand];
-			
-			//If the adder is not the same as the last, add the track
-			if (temp.added_by_id != previous_adder) break;
-		}
-        
-        
+        // Determine a random index with which we will swap current element
+        const rand = random_number(0, array.length - 1);
+        const temp = array[rand];
+
         // Swap the current item with the randomly picked item
         array[rand] = array[i];
         array[i] = temp;
@@ -139,12 +129,10 @@ function batch_swap_shuffle(array, size) {
     // Generate shuffled batches from the provided array
     const batches = [];
     const iterations = Math.ceil(array.length / size);
-	prev_adder = null;
     for (let i = 0; i < iterations; i++) {
         const batch = array.slice(i * size, i * size + size);
-        swap_shuffle(batch, prev_adder);
+        swap_shuffle(batch);
         batches.push(batch);
-		prev_adder = batch[batch.length - 1];
     }
 
     // Return the batches as a single array
